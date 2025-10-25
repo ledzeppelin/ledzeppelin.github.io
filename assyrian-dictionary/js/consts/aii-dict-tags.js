43fafa3420dbd0af874cc51199537cf08e68d698
sharrukin <4949540+ledzeppelin@users.noreply.github.com>
2025-10-24 20:23:48 -0700

    oct 20 code

--- assyrian-dictionary/js/consts/aii-dict-tags.js
+++ assyrian-dictionary/js/consts/aii-dict-tags.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a50e96d1f2ff7bae489352457b92554c39738e0653b39223b3455e79a35580cd
-size 15802
+oid sha256:1ace826d32cb53aaea548f9e3daf2a143cc24d0d7d05b90f7ce910b75db53371
+size 15978
